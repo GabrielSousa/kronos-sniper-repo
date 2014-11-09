e77425e1b8569a315113a217553498ee136798d4
callebe <callebe.barbosa@gmail.com>
2014-11-09 01:33:53 -0200

    Mudanças

--- js/calendario.js
+++ js/calendario.js
@@ -14,45 +14,45 @@ $.getScript('http://arshaw.com/js/fullcalendar-1.6.4/fullcalendar/fullcalendar.m
     editable: true,
     events: [
       {
-        title: 'All Day Event',
+        title: 'Atendimento Sr. João',
         start: new Date(y, m, 1)
       },
       {
-        title: 'Long Event',
+        title: 'Folga',
         start: new Date(y, m, d-5),
         end: new Date(y, m, d-2)
       },
       {
         id: 999,
-        title: 'Repeating Event',
+        title: 'Radiografia',
         start: new Date(y, m, d-3, 16, 0),
         allDay: false
       },
       {
         id: 999,
-        title: 'Repeating Event',
+        title: 'Exame',
         start: new Date(y, m, d+4, 16, 0),
         allDay: false
       },
       {
-        title: 'Meeting',
+        title: 'Exame',
         start: new Date(y, m, d, 10, 30),
         allDay: false
       },
       {
-        title: 'Lunch',
+        title: 'Sonografia',
         start: new Date(y, m, d, 12, 0),
         end: new Date(y, m, d, 14, 0),
         allDay: false
       },
       {
-        title: 'Birthday Party',
+        title: 'Aula',
         start: new Date(y, m, d+1, 19, 0),
         end: new Date(y, m, d+1, 22, 30),
         allDay: false
       },
       {
-        title: 'Click for Google',
+        title: 'Infecção',
         start: new Date(y, m, 28),
         end: new Date(y, m, 29),
         url: 'http://google.com/'
